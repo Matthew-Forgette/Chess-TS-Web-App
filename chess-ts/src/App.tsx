8d22153d1283bcb71cbdf3844e8986d7015de5d7
matthew-forgette <mj0723@gmail.com>
2021-02-25 23:02:03 -0500

    finally found a satisfactory solution to the responsiveness of the chessboardjsx

--- chess-ts/src/App.tsx
+++ chess-ts/src/App.tsx
@@ -1,23 +1,54 @@
 import React from "react";
 import "./App.css";
 // components
-import { MobileApp, TabletApp, DesktopApp } from "./components";
+import Chessboard from 'chessboardjsx'
+import { NavComponent } from './components/NavComponent'
 const Chess = require("chess.js");
+
+// logic that controls the width of the gameboard based on the width of the viewport
+// I had to do it this way because chessboardjsx doesn't respond to css width
+let gameboardWidthLogic: number;
+const vw: number = window.innerWidth;
+const vh: number = window.innerHeight;
+if(vw>vh) { // horizontal displays
+    if (vw < 2000 && vw > 1601) { 
+        gameboardWidthLogic = vw / 2 - 100;
+    } else if (vw < 1600 && vw > 1201) { 
+        gameboardWidthLogic = vw / 2 ;
+    } else if (vw < 1200 && vw > 1001) { 
+        gameboardWidthLogic = vw / 2 - 30;
+    } else if (vw < 1000 && vw > 801) { 
+        gameboardWidthLogic = vw / 2 + 50;
+    } else if (vw < 800 && vw > 501) { 
+        gameboardWidthLogic = vw / 2;
+    } else {
+        gameboardWidthLogic = vw / 2;
+    }
+} else if(vh>vw) { // vertical displays
+    if (vw < 700 && vw > 601) {
+        gameboardWidthLogic = vw - (vw / 10)
+    } else if (vw < 600 && vw > 501) {
+        gameboardWidthLogic = vw - (vw / 15)
+    } else if (vw < 500 && vw > 401) {
+        gameboardWidthLogic = vw - (vw / 20)
+    } else if (vw < 400 && vw > 301) {
+        gameboardWidthLogic = vw - (vw / 25)
+    } else if (vw < 300) {
+        gameboardWidthLogic = vw - (vw / 30)
+    }
+}
 
 const  App:React.FC = () => {
   // const chess: any = new Chess();
   console.log(window.innerWidth);
   return (
     <main className="App">
-      <DesktopApp  />
-      {/* ternary designed to render specific components based on viewport width. I did it this way because the chessboardjsx library only allows you to adjust the size of the board with the width prop. this allows me to customize each component based on its relationship to the viewport size */}
-      {/* {window.innerWidth < 768 ? (
-        <MobileApp />
-      ) : window.innerWidth > 760 && window.innerWidth < 1024 ? (
-        <TabletApp />
-      ) : (
-        <DesktopApp />
-      )} */}
+      <NavComponent />
+      <Chessboard
+        position='start'
+        width={gameboardWidthLogic}
+      />
+      
     </main>
   );
 }
